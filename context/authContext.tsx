177dde2ccfc473caa0159037a463d7c8d8b42a2d
DOMAA21 <clashecurry21@gmail.com>
2024-03-30 14:32:50 +0800

    todo: persist login

--- context/authContext.tsx
+++ context/authContext.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+"use client"
+import React, {useEffect} from 'react';
 import { createContext, useContext, useState } from 'react';
+import jsonwebtoken from "jsonwebtoken";
+import { getCookie } from 'cookies-next';
 
 interface User {
   email: string;
@@ -23,6 +26,22 @@ const AuthContext = createContext<AuthContextType>({
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const token = getCookie('token');
+    console.log(document.cookie);
+    console.log(token)
+    if (token) {
+      try {
+        const decodedToken = jsonwebtoken.decode(token);
+        setUser(decodedToken);
+        setIsAuthenticated(true);
+      } catch (error) {
+        // Handle invalid token
+        console.error('Invalid token:', error);
+      }
+    }
+  }, []);
 
   const login = (credentials: User) => {
     setIsAuthenticated(true);
